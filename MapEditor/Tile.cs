f801950de8fa1fde78cb876bec1b34dc5d5fe408
tinifyphan <tinify.phan@gmail.com>
2016-10-20 15:04:16 +0700

    TileSet and Interface info

---
+++ MapEditor/Tile.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapEditor
+{
+    //INotifyPropertyChanged chỉ có duy nhất một thành viên là event mang tên PropertyChanged. 
+    //Khi định nghĩa một class để dùng cho binding,
+    //cần kích hoạt event này trong setter cho mỗi property trong class đó.
+
+    class Tile : INotifyPropertyChanged
+    {
+        #region FIELDS_PROPERTIES
+        //id cho tile
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { setProperty(ref _id, value, "Id"); }
+        }
+
+        //name Tile
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { setProperty(ref _name, value, "Name"); }
+        }
+
+        //srcRect dùng để lưu thông tin để vẻ của tile lên màn hình
+        private Rectangle _srcRect;
+
+        public Rectangle SrcRect
+        {
+            get { return _srcRect; }
+            set { setProperty(ref _srcRect, value, "SrcRect"); }
+        }
+
+        //ảnh gồm nhiều tile,khi vẽ cắt ảnh bằng srcRect như sprite
+        Image _image;
+
+        public Image Image
+        {
+            get { return _image; }
+            set { setProperty(ref _image, value, "Image"); }
+        }
+
+        //implment event PropertyChanged cho class
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void setProperty<T>(ref T obj, T value, string propertyName)
+        {
+            if (object.Equals(obj, value))
+                return;
+            obj = value;
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region CONSTRCUTOR 
+        public Tile(Image image, Rectangle srcRect, int id)
+        {
+            Id = id;
+            SrcRect = srcRect;
+            Image = image;
+            Name = "tile_" + id.ToString();
+        }
+        #endregion
+
+        #region METHODS
+        //trả về ảnh đã cắt bằng srcRect
+        public Bitmap getBitmap()
+        { 
+            return ((_image as Bitmap).Clone(SrcRect,_image.PixelFormat));
+        }
+        
+        //vẽ bitmap ra màn hình
+        public void draw(Graphics graphics, Point position, Size size) 
+        {
+            graphics.DrawImage(_image,new Rectangle(position,size),SrcRect,GraphicsUnit.Pixel);
+        }
+        #endregion
+
+    }
+}
---
+++ MapEditor/TileSet.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+//chi tiết INotifyPropertyChanged, INotifyCollectionChanged
+//http://www.codeproject.com/Articles/42536/List-vs-ObservableCollection-vs-INotifyPropertyCha
+namespace MapEditor
+{
+    //INotifyPropertyChanged cung cấp event PropertyChanged mỗi khi giá trị các properties của 
+    //TileSet bị thay đổi và cập nhật lại UI
+    class TileSet : INotifyPropertyChanged, INotifyCollectionChanged
+    {
+        #region FIELDS_PROPERTIES
+        ////FIELDS
+
+        //chiều rộng của 1 tile
+        private int _widthtile;            
+        //Chiều cao của 1 tile
+        private int _heighttile;              
+        //tên tile
+        private string _filename;
+        //Số cột của tileset
+        private int _columns;
+        //Số hàng của tileset
+        private int _rows;
+        //Danh sách các Tile được cắt ra từ tileSet
+        private IList<Tile> _listTiles;
+        
+        //PROPERTIES
+        public int Widthtile
+        {
+            get { return _widthtile; }
+            set { this.setProperty(ref _widthtile, value, "Widthtile"); }
+        }
+
+        public int Heighttile
+        {
+            get { return _heighttile; }
+            set { this.setProperty(ref _heighttile, value, "Heighttile"); }
+        }
+
+        public string Filename
+        {
+            get { return _filename; }
+            set { setProperty(ref _filename, value, "FileName"); }
+        }
+
+        public int Columns
+        {
+            get { return _columns; }
+            set { setProperty(ref _columns, value, "Columns"); }
+        }
+
+        public int Rows
+        {
+            get { return _rows; }
+            set { setProperty(ref _rows, value, "Rows"); }
+        }
+
+        public Image Image { get; set; }
+
+        public ObservableCollection<Tile> ListTiles
+        {
+            get { return _listTiles as ObservableCollection<Tile>; }
+            set
+            {
+                if (_listTiles != value)
+                {
+                    if (_listTiles != null)
+                        (_listTiles as ObservableCollection<Tile>).CollectionChanged -= this.CollectionChanged;
+                    value.CollectionChanged += CollectionChanged;
+                    setProperty(ref _listTiles, value, "ListTiles");
+                }
+            }
+        }
+
+        
+        //EVENTS
+
+        //dùng để gán event CollectionChanged cho listTiles as ObservableCollection
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        //tạo event dùng cho interface INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private void setProperty<T>(ref T obj, T value, string propertyName)
+        {
+            if (Object.Equals(obj, value))
+                return;
+            obj = value;
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region CONSTRUCTOR
+
+        public TileSet()
+        {
+            this.ListTiles = new ObservableCollection<Tile>();
+            
+            this.PropertyChanged += TileSet_PropertyChanged;
+        }
+
+        private void TileSet_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Widthtile")
+            {
+                if (Image != null)
+                    this.Columns = (Widthtile == 0) ? 1 : (Image.Width / Widthtile);
+            }
+            else if (e.PropertyName == "Heighttile")
+            {
+                if (Image != null)
+                    this.Rows = (Heighttile == 0) ? 1 : (Image.Height / Heighttile);
+            }
+            else if (e.PropertyName == "Image")
+            {
+                this.Columns = (Widthtile == 0) ? 1 : (Image.Width / Widthtile);
+                this.Rows = (Heighttile == 0) ? 1 : (Image.Height / Heighttile);
+            }
+        }
+
+        #endregion
+    }
+}
